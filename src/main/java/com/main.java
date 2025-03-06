60bbbd437325ea0fe7c14121c12f17e0d073b2dc
ManuelJose05 <manueljoseliebana05@gmail.com>
2025-03-06 17:23:08 +0100

    add: Persistencia Local(leer ficheros locales)

---
+++ src/main/java/com/main.java
@@ -0,0 +1,11 @@
+package com;
+
+import com.persistence.Persistence;
+
+public class main {
+    public static void main(String[] args) {
+        Persistence persistence = new Persistence();
+
+        persistence.leeInformes();
+    }
+}
